d5e04965ad54f3c3d6fc31f22f02caae0158ebed
Masaki.Nakano <admin@namachan10777.dev>
2023-11-25 10:02:25 +0900

    remove isucondifion's nginx conf

--- itamae/cookbooks/nginx/default.rb
+++ itamae/cookbooks/nginx/default.rb
@@ -8,8 +8,3 @@ remote_file '/etc/nginx/nginx.conf' do
   notifies :reload, 'service[nginx]'
 end
 
-remote_file '/etc/nginx/sites-available/isucondition.conf' do
-  user 'root'
-  mode '644'
-  notifies :reload, 'service[nginx]'
-end
