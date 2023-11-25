6e7843348a0308883f0d09056495e196119ba2a1
Masaki.Nakano <admin@namachan10777.dev>
2023-11-25 09:33:44 +0900

    remove isucondition

--- itamae/roles/default.rb
+++ itamae/roles/default.rb
@@ -1,5 +1,4 @@
 include_recipe '../cookbooks/common'
 include_recipe '../cookbooks/rust'
 include_recipe '../cookbooks/monitoring'
-include_recipe '../cookbooks/isucondition'
 include_recipe '../cookbooks/nginx'
