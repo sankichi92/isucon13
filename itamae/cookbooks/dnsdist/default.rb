c5cd08ecfea688338f4c180ff376e95ccc0c14e0
sksat <sksat@sksat.net>
2023-11-25 15:24:25 +0900

    use dnsdist for DNS proxy

--- itamae/cookbooks/dnsdist/default.rb
+++ itamae/cookbooks/dnsdist/default.rb
@@ -1,7 +1,11 @@
 package 'dnsdist'
 
+service 'dnsdist' do
+  action :nothing
+end
+
 remote_file '/etc/dnsdist/dnsdist.conf' do
   user 'root'
   mode '644'
-  #notifies :restart, 'service[dnsdist]'
+  notifies :restart, 'service[dnsdist]'
 end
