e2d85080ab9f2e56c146ceef5e85c1050c548f15
sksat <sksat@sksat.net>
2023-11-25 12:11:00 +0900

    add isupipe-rust cookbook

---
+++ itamae/cookbooks/isupipe/default.rb
@@ -0,0 +1,33 @@
+service 'isupipe-rust' do
+  user 'root'
+  action :nothing
+end
+
+execute 'systemctl daemon-reload' do
+  user 'root'
+  action :nothing
+end
+
+directory '/home/isucon/webapp/rust/target/x86_64-unknown-linux-gnu/release' do
+  user 'isucon'
+end
+
+remote_file '/home/isucon/webapp/rust/target/x86_64-unknown-linux-gnu/release/isupipe' do
+  user 'root'
+  mode '755'
+  source '../../../webapp/rust/target/x86_64-unknown-linux-gnu/release/isupipe'
+  notifies :restart, 'service[isupipe-rust]'
+end
+
+remote_file '/etc/systemd/system/isupipe-rust.service' do
+  user 'root'
+  mode '644'
+  notifies :run, 'execute[systemctl daemon-reload]'
+  notifies :restart, 'service[isupipe-rust]'
+end
+
+# それぞれのホストで変更した方が柔軟説
+#remote_file '/home/isucon/env.sh' do
+#  user 'isucon'
+#  mode '644'
+#end
